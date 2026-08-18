d7a2ca5d8a6f1cd3643e7a065a9bcdbf4757b4dd
dbarzin <dbarzin@gmail.com>
2026-08-18 08:11:54 +0200

    rebuild

---
+++ public/build/assets/bpmn-show-BYXjdSW6.js
@@ -0,0 +1,2 @@
+import{t as e}from"./bpmn-editor-zeztdhVL.js";var t=document.getElementById(`graph-container`);if(!t)throw Error(`#graph-container introuvable`);var n=e(t,{ui:`none`,readOnly:!0,onNavigate:e=>{window.location.href=e}});n.on(`error`,e=>console.error(`[bpmn-show]`,e)),window.loadGraph=e=>n.loadXml(e),window.getXMLGraph=()=>n.getXml();
+//# sourceMappingURL=bpmn-show-BYXjdSW6.js.map
