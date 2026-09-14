29fe2b58007e419fb6a611032c4028f81fc8e9c6
dbarzin <dbarzin@gmail.com>
2026-09-14 16:38:03 +0200

    improve role editor

--- resources/views/admin/roles/partials/_checkbox.blade.php
+++ resources/views/admin/roles/partials/_checkbox.blade.php
@@ -1,12 +1,14 @@
 @php
-    $actionId    = $action[0];
-    $actionLabel = $action[1];
-    $isChecked   = in_array($actionId, old('permissions', [])) || ($role && $role->permissions->contains($actionId));
-    $sizeClass   = $disabled ? '' : ' form-switch-lg';
+    $actionId     = $action[0];
+    $actionLabel  = $action[1];
+    $isChecked    = in_array($actionId, old('permissions', [])) || ($role && $role->permissions->contains($actionId));
+    $sizeClass    = $disabled ? '' : ' form-switch-lg';
+    $normalizedAction = $actionLabel == 'access' ? 'list' : $actionLabel;
 @endphp
 <div class="form-switch{{ $sizeClass }}">
     <input class="form-check-input" type="checkbox" name="permissions[]"
         data-check="{{ $permission['name'] }}"
+        data-action="{{ $normalizedAction }}"
         id="perm_{{ $actionId }}"
         value="{{ $actionId }}"
         @disabled($disabled)
--- resources/views/admin/roles/partials/_section_header.blade.php
+++ resources/views/admin/roles/partials/_section_header.blade.php
@@ -3,7 +3,7 @@
     $hdrChecked  = in_array($hdrActionId, old('permissions', [])) || ($role && $role->permissions->contains($hdrActionId));
     $hdrSize     = $disabled ? '' : ' form-switch-lg';
 @endphp
-<div class="card-header">
+<div class="card-header d-flex justify-content-between align-items-center">
     <div class="form-switch{{ $hdrSize }}">
         <input class="form-check-input" type="checkbox" name="permissions[]"
             data-check="{{ $permission['name'] }}"
@@ -13,4 +13,14 @@
             @checked($hdrChecked)>
         <label class="form-check-label"><b>{{ $label }}</b></label>
     </div>
+    @unless($disabled)
+        <div class="btn-group btn-group-sm perm-toolbar" role="group">
+            <button type="button" class="btn btn-outline-secondary perm-toolbar-btn" data-scope="all">All</button>
+            <button type="button" class="btn btn-outline-secondary perm-toolbar-btn" data-scope="list">list</button>
+            <button type="button" class="btn btn-outline-secondary perm-toolbar-btn" data-scope="show">show</button>
+            <button type="button" class="btn btn-outline-secondary perm-toolbar-btn" data-scope="create">create</button>
+            <button type="button" class="btn btn-outline-secondary perm-toolbar-btn" data-scope="edit">edit</button>
+            <button type="button" class="btn btn-outline-secondary perm-toolbar-btn" data-scope="delete">delete</button>
+        </div>
+    @endunless
 </div>
